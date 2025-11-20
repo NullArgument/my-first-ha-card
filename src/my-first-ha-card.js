2f8dc3b17189299b982972de1dfe627efe1381bc
NullArgument <37764522+NullArgument@users.noreply.github.com>
2025-11-19 21:40:57 -0500

    Added My First HA Card

---
+++ src/my-first-ha-card.js
@@ -0,0 +1,45 @@
+class MyFirstHACard extends HTMLElement
+{
+    set hass(hass)
+    {
+        if (!this.content)
+        {
+            this.innerHTML = `
+                <ha-card header="My-First-Card">
+                    <div class="card-content"></div>
+                </ha-card>
+            `;
+
+            this.content = this.querySelector("div");
+        }
+
+        const entityId = this.content.entity;
+        const state = hass.states(entityId);
+        const stateStr = state ? state.state : "unavailable";
+
+        this.content.innerHTML = `
+            The state of ${entityId} is ${stateStr}!
+            <br><br>
+            [add additional content here]
+        `;
+    }
+
+    getCardSize()
+    {
+        //---- Not sure what 3 means... ----
+        return 3;
+    }
+
+    getGridOptions()
+    {
+        return
+        {
+            rows: 3,
+            columns: 6,
+            min_rows: 3,
+            max_rows: 3
+        };
+    }
+}
+
+customElements.define("my-first-ha-card", MyFirstHACard);
